14c4a34db0a1209fc54d7b4b92b3121a6c75b6ee
awells111 <awells111@gmail.com>
2017-05-20 15:58:59 -0400

    WorldTrotter now only allows one decimal

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -8,18 +8,72 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController {
+class ConversionViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelsiusLabel()
+    }
+    
+    var farenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let farenheitValue = farenheitValue {
+            return farenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
     
     @IBAction func farenheitFieldEditingChanged(_ textField: UITextField) {
         
-        if let text = textField.text, !text.isEmpty {
-            celsiusLabel.text = textField.text
+        if let text = textField.text, let value = Double(text) {
+            farenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            farenheitValue = nil
+        }
+        
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
         } else {
             celsiusLabel.text = "???"
         }
+    }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn rage: NSRange,
+                   replacementString string: String) -> Bool {
         
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        
+        if existingTextHasDecimalSeparator != nil,
+            replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
     }
 }
