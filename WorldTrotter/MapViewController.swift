1f0dd3b01e71712cc16be1c67aa24c64ceb0b64f
awells111 <awells111@gmail.com>
2017-05-23 10:59:55 -0400

    Added Map View and Tab Bar Controller

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Alex Wells on 5/23/17.
+//  Copyright © 2017 Alex C Wells. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+                print("MapViewController loaded its view.")
+    }
+}
